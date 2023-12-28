438891a4e97ba30e5afd5d7373d5d43d38895d28
Alen Chacko <alenchacko.off@gmail.com>
2023-12-28 21:02:16 +0530

    file reading synchronously

--- basics/readingTerminal.js
+++ basics/readingTerminal.js
@@ -1,11 +1,20 @@
 // reading values from terminal
+
+// importing module
 const readLine = require('readline')
 const rl = readLine.createInterface({
     input:process.stdin,
     output:process.stdout
 })
 
+// creating readline interface
 rl.question("Enter your name : ",(name)=>{
     console.log(`Your name is ${name}`)
     rl.close()
+})
+
+// listening to the close event
+rl.on('close',()=>{
+    console.log('Interface closed')
+    process.exit(0)
 })
---
+++ basics/synchronousReading.js
@@ -0,0 +1,12 @@
+// Reading files in a synchronous method
+const fs = require('fs')
+
+// starting
+console.log('starting')
+
+// reading
+let inpTxt = fs.readFileSync('./files/input.txt','utf-8')
+console.log(inpTxt)
+
+// finished
+console.log('finished')
